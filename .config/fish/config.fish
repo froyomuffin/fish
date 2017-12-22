e47e5c56fd112a7daac2ca5cc458d6e47a598d81
Thomas Wang <thomas.wang@shopify.com>
2017-12-22 09:35:45 -0500

    Clean up functions and added dir-exists

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,22 @@
-set -x PATH ~/bin/ ~/git/toolbox/bin/ $PATH
-
-function check-cmd
-    which $argv[1] > /dev/null
-    if [ $status ]
-      return 0
-    else
-      return 1
-    end
+function invert
+  if [ $argv[1] = 0 ]
+    return 1
+  else
+    return 0
+  end
 end
+
+function function-exists
+  which $argv[1] > /dev/null
+  invert $status
+end
+
+function dir-exists
+  test -d $argv[1]
+  invert $status
+end
+
+set -x PATH ~/bin/ ~/git/toolbox/bin/ $PATH
 
 # Vi!
 #fish_vi_key_bindings
@@ -17,29 +26,29 @@ fish_default_key_bindings
 #set -g hist_ignore_dups
 
 # Linux applications
-#check-cmd pulseaudio ;and test -z (pidof pulseaudio) ;and echo 'Starting pulseaudio' ;and pulseaudio --start
-check-cmd startx ;and [ $DISPLAY ] ;and test -z $DISPLAY ;and test $XDG_VTNR -eq 1 ;and exec startx
-check-cmd xdg-open ;and alias open 'xdg-open'
+#function-exists pulseaudio ;and test -z (pidof pulseaudio) ;and echo 'Starting pulseaudio' ;and pulseaudio --start
+function-exists startx ;and [ $DISPLAY ] ;and test -z $DISPLAY ;and test $XDG_VTNR -eq 1 ;and exec startx
+function-exists xdg-open ;and alias open 'xdg-open'
 
 # Set clipboard to c and v
-check-cmd xclip ;and alias c 'xclip -selection clipboard' ;and alias v 'xclip -o -selection clipboard'
-check-cmd pbcopy ;and alias c 'pbcopy'; check-cmd pbpaste ;and alias v 'pbpaste'
+function-exists xclip ;and alias c 'xclip -selection clipboard' ;and alias v 'xclip -o -selection clipboard'
+function-exists pbcopy ;and alias c 'pbcopy'; function-exists pbpaste ;and alias v 'pbpaste'
 
 # Fix ctags in macOS
-#check-cmd brew ;and check-cmd ctags ;and alias ctags (brew --prefix ctags)'/bin/ctags'
+#function-exists brew ;and function-exists ctags ;and alias ctags (brew --prefix ctags)'/bin/ctags'
 
 # Default tag
-check-cmd ctags ;and alias tag 'ctags -R'
+function-exists ctags ;and alias tag 'ctags -R'
 
 # Neovim
 set -x EDITOR nvim
-check-cmd nvim ;and alias vim 'nvim'
+function-exists nvim ;and alias vim 'nvim'
 
 # Exa
-check-cmd exa ;and alias ls 'exa'
+function-exists exa ;and alias ls 'exa'
 
 # fd
-check-cmd fd ;and alias find 'fd'
+function-exists fd ;and alias find 'fd'
 
 source ~/.private.fish
 #status --is-login; and status --is-interactive; and exec byobu-launcher
